48b13c68730a13abc982f0b9015ef583fd80926d
SA0091 <sasafinalitraffic@gmail.ccom>
2022-05-31 06:39:27 +0600

    Starbucks Landing Page Without Responsive

---
+++ js/active.js
@@ -0,0 +1,10 @@
+// For Ice Cream Image & Circle > Color Change
+
+
+function selectIceCream(icebarImages) {
+    document.querySelector('#iceCreamshape').src = icebarImages;
+};
+
+function changeColor(color) {
+    document.querySelector('.circle').style.backgroundColor = color;
+};
